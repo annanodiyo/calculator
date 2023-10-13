713919af760b3b27aeb5b165a6b2716d3ab88cc5
annanodiyo <annanodiyo@gmail.com>
2023-10-13 11:15:52 +0300

    Calculator app

---
+++ calc.js
@@ -0,0 +1,13 @@
+function display(datvalue){
+    document.getElementById("result").value += datvalue
+}
+function myInput(event){
+    if ((event.key == '0' || event.key == '1'
+    || event.key == '2' || event.key == '3'
+    || event.key == '4' || event.key == '5'
+    || event.key == '6' || event.key == '7'
+    || event.key == '8' || event.key == '9'
+    || event.key == '+' || event.key == '-'
+    || event.key == '*' || event.key == '/')
+    document.getElementById("result").value += event.key;
+}
