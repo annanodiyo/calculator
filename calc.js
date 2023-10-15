39d743b23aec50642e4d522fc8ab070a76533659
annanodiyo <annanodiyo@gmail.com>
2023-10-15 15:29:58 +0300

    working calculator

--- calc.js
+++ calc.js
@@ -1,29 +1,13 @@
-
-function display(datvalue){
-    document.getElementById("result").value += datvalue
+function display(val){
+    document.getElementById('display').value += val;
+    return val;
 }
-function myFunction(event){
-    if (event.key == '0' || event.key == '1'
-    || event.key == '2' || event.key == '3'
-    || event.key == '4' || event.key == '5'
-    || event.key == '6' || event.key == '7'
-    || event.key == '8' || event.key == '9'
-    || event.key == '+' || event.key == '-'
-    || event.key == '*' || event.key == '/')
-
-    document.getElementById("result").value += event.key;
+function calculate(){
+    let x = document.getElementById('display').value;
+    let y = eval(x);
+    document.getElementById('display').value = y;
+    return y;
 }
-var cal = document.getElementById("calculate");
-    cal.onkeyup = function (event) {
-        if (event.keyCode === 13) {
-            console.log("Enter");
-            let x = document.getElementById("result").value
-            console.log(x);
-            solve();
-        }
-}
-function solve() {
-    let x = document.getElementById("result").value
-    let y = math.evaluate(x)
-    document.getElementById("result").value = y
+function clear(){
+    document.getElementById('display').value = "";
 }
